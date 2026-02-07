0bc851c5401b09c304d5035fa3a6d2e1a75c7e40
Keisec <keisec.5910@gmail.com>
2026-02-07 10:56:37 +0200

    fixed a bug where a renamed file would crash the plugin

--- lua/lf/config.lua
+++ lua/lf/config.lua
@@ -8,143 +8,143 @@ local utils = require("lf.utils")
 ---@field group integer Autocmd id
 ---@field __loaded boolean
 local Config = {
-    data = {},
-    __loaded = false,
+	data = {},
+	__loaded = false,
 }
 
 ---@type Lf.Config
 local default = {
-    default_cmd = "lf",
-    default_action = "drop",
-    default_actions = {
-        ["<C-t>"] = "tabedit",
-        ["<C-x>"] = "split",
-        ["<C-v>"] = "vsplit",
-        ["<C-o>"] = "tab drop",
-        ["<C-e>"] = "edit",
-        ["<C-g>"] = "argedit",
-    },
-    winblend = 10,
-    dir = "",
-    direction = "float",
-    border = "double",
-    height = fn.float2nr(fn.round(0.75 * o.lines)),
-    width = fn.float2nr(fn.round(0.75 * o.columns)),
-    escape_quit = false,
-    focus_on_open = true,
-    mappings = true,
-    tmux = false,
-    default_file_manager = false,
-    disable_netrw_warning = true,
-    highlights = {
-        Normal = {link = "Normal"},
-        FloatBorder = {link = "FloatBorder"},
-    },
-    count = nil,
-    env = {
-        clear = false,
-        vars = {}, -- NOTE: this doesn't work for now
-    },
-    -- Layout configurations
-    layout_mapping = "<A-u>",
-    views = {
-        {width = 0.800, height = 0.800},
-        {width = 0.600, height = 0.600},
-        {width = 0.950, height = 0.950},
-        {width = 0.500, height = 0.500, col = 0, row = 0},
-        {width = 0.500, height = 0.500, col = 0, row = 0.5},
-        {width = 0.500, height = 0.500, col = 0.5, row = 0},
-        {width = 0.500, height = 0.500, col = 0.5, row = 0.5},
-    },
+	default_cmd = "lf",
+	default_action = "drop",
+	default_actions = {
+		["<C-t>"] = "tabedit",
+		["<C-x>"] = "split",
+		["<C-v>"] = "vsplit",
+		["<C-o>"] = "tab drop",
+		["<C-e>"] = "edit",
+		["<C-g>"] = "argedit",
+	},
+	winblend = 10,
+	dir = "",
+	direction = "float",
+	border = "double",
+	height = fn.float2nr(fn.round(0.75 * o.lines)),
+	width = fn.float2nr(fn.round(0.75 * o.columns)),
+	escape_quit = false,
+	focus_on_open = true,
+	mappings = true,
+	tmux = false,
+	default_file_manager = false,
+	disable_netrw_warning = true,
+	highlights = {
+		Normal = { link = "Normal" },
+		FloatBorder = { link = "FloatBorder" },
+	},
+	count = nil,
+	env = {
+		clear = false,
+		vars = {}, -- NOTE: this doesn't work for now
+	},
+	-- Layout configurations
+	layout_mapping = "<A-u>",
+	views = {
+		{ width = 0.800, height = 0.800 },
+		{ width = 0.600, height = 0.600 },
+		{ width = 0.950, height = 0.950 },
+		{ width = 0.500, height = 0.500, col = 0, row = 0 },
+		{ width = 0.500, height = 0.500, col = 0, row = 0.5 },
+		{ width = 0.500, height = 0.500, col = 0.5, row = 0 },
+		{ width = 0.500, height = 0.500, col = 0.5, row = 0.5 },
+	},
 }
 
 ---Validate configuration values
 ---@param cfg Lf.Config existing configuration options
 ---@return Lf.Config
 local function validate(cfg)
-    vim.validate("default_cmd", cfg.default_cmd, "string", false)
-    vim.validate("default_action", cfg.default_action, "string", false)
-    vim.validate("default_actions", cfg.default_actions, "table", false)
-    vim.validate("winblend", cfg.winblend, {"number", "string"}, false)
-    vim.validate("dir", cfg.dir, "string", false)
-    vim.validate("direction", cfg.direction, "string", false)
-    vim.validate("border", cfg.border, {"string", "table"}, false)
-    vim.validate("height", cfg.height, {"number", "string"}, false)
-    vim.validate("width", cfg.width, {"number", "string"}, false)
-    vim.validate("escape_quit", cfg.escape_quit, "boolean", false)
-    vim.validate("focus_on_open", cfg.focus_on_open, "boolean", false)
-    vim.validate("mappings", cfg.mappings, "boolean", false)
-    vim.validate("tmux", cfg.tmux, "boolean", false)
-    vim.validate("default_file_manager", cfg.default_file_manager, "boolean", false)
-    vim.validate("disable_netrw_warning", cfg.disable_netrw_warning, "boolean", false)
-    vim.validate("highlights", cfg.highlights, "table", false)
-    vim.validate("count", cfg.count, "number", true)
-    vim.validate("env", cfg.env, "table", false)
-    vim.validate("env_vars", cfg.env.vars, "table", false)
-    vim.validate("env_clear", cfg.env.clear, "boolean", false)
-    vim.validate("layout_mapping", cfg.layout_mapping, "string", false)
-    vim.validate("views", cfg.views, "table", false)
+	vim.validate("default_cmd", cfg.default_cmd, "string", false)
+	vim.validate("default_action", cfg.default_action, "string", false)
+	vim.validate("default_actions", cfg.default_actions, "table", false)
+	vim.validate("winblend", cfg.winblend, { "number", "string" }, false)
+	vim.validate("dir", cfg.dir, "string", false)
+	vim.validate("direction", cfg.direction, "string", false)
+	vim.validate("border", cfg.border, { "string", "table" }, false)
+	vim.validate("height", cfg.height, { "number", "string" }, false)
+	vim.validate("width", cfg.width, { "number", "string" }, false)
+	vim.validate("escape_quit", cfg.escape_quit, "boolean", false)
+	vim.validate("focus_on_open", cfg.focus_on_open, "boolean", false)
+	vim.validate("mappings", cfg.mappings, "boolean", false)
+	vim.validate("tmux", cfg.tmux, "boolean", false)
+	vim.validate("default_file_manager", cfg.default_file_manager, "boolean", false)
+	vim.validate("disable_netrw_warning", cfg.disable_netrw_warning, "boolean", false)
+	vim.validate("highlights", cfg.highlights, "table", false)
+	vim.validate("count", cfg.count, "number", true)
+	vim.validate("env", cfg.env, "table", false)
+	vim.validate("env_vars", cfg.env.vars, "table", false)
+	vim.validate("env_clear", cfg.env.clear, "boolean", false)
+	vim.validate("layout_mapping", cfg.layout_mapping, "string", false)
+	vim.validate("views", cfg.views, "table", false)
 
-    cfg.winblend = tonumber(cfg.winblend) --[[@as number]]
-    cfg.height = tonumber(cfg.height) --[[@as number]]
-    cfg.width = tonumber(cfg.width) --[[@as number]]
-    return cfg
+	cfg.winblend = tonumber(cfg.winblend) --[[@as number]]
+	cfg.height = tonumber(cfg.height) --[[@as number]]
+	cfg.width = tonumber(cfg.width) --[[@as number]]
+	return cfg
 end
 
 ---Set a configuration passed as a function argument (not through `setup`)
 ---@param cfg? Lf.Config configuration options
 ---@return Lf.Config
 function Config:override(cfg)
-    if type(cfg) == "table" then
-        self.data = vim.tbl_deep_extend("force", self.data, cfg) --[[@as Lf.Config]]
-        self.data = validate(self.data)
-    end
-    return self.data
+	if type(cfg) == "table" then
+		self.data = vim.tbl_deep_extend("force", self.data, cfg) --[[@as Lf.Config]]
+		self.data = validate(self.data)
+	end
+	return self.data
 end
 
 ---Merge a configuration without updating the global state
 ---@param cfg? Lf.Config configuration options
 ---@return Lf.Config
 function Config:merge(cfg)
-    local data = vim.deepcopy(self.data)
-    if type(cfg) == "table" then
-        data = vim.tbl_deep_extend("force", data, cfg) --[[@as Lf.Config]]
-        data = validate(data)
-    end
-    return data
+	local data = vim.deepcopy(self.data)
+	if type(cfg) == "table" then
+		data = vim.tbl_deep_extend("force", data, cfg) --[[@as Lf.Config]]
+		data = validate(data)
+	end
+	return data
 end
 
 ---Return the configuration
 ---@param key? string
 ---@return Lf.Config
 function Config:get(key)
-    if key then
-        return self.data[key]
-    end
-    return self.data
+	if key then
+		return self.data[key]
+	end
+	return self.data
 end
 
 ---Initialize the default configuration
 function Config.init()
-    if Config.__loaded then
-        return
-    end
+	if Config.__loaded then
+		return
+	end
 
-    local lf = require("lf")
-    -- Keep options from the `lf.setup()` call
-    Config.data = vim.tbl_deep_extend("keep", lf.__conf or {}, default) --[[@as Lf.Config]]
-    Config.data = validate(Config.data)
-    lf.__conf = nil
-    Config.__loaded = true
+	local lf = require("lf")
+	-- Keep options from the `lf.setup()` call
+	Config.data = vim.tbl_deep_extend("keep", lf.__conf or {}, default) --[[@as Lf.Config]]
+	Config.data = validate(Config.data)
+	lf.__conf = nil
+	Config.__loaded = true
 end
 
 return setmetatable(Config, {
-    __index = function(self, key)
-        return rawget(self, key)
-    end,
-    __newindex = function(_self, key, val)
-        utils.warn(("do not set invalid config values: %s => %s"):format(key, val))
-    end,
+	__index = function(self, key)
+		return rawget(self, key)
+	end,
+	__newindex = function(_self, key, val)
+		utils.warn(("do not set invalid config values: %s => %s"):format(key, val))
+	end,
 })
 
 ---@alias Lf.border.generic {[1]:string,[2]:string,[3]:string,[4]:string,[5]:string,[6]:string,[7]:string,[8]:string}
--- lua/lf/main.lua
+++ lua/lf/main.lua
@@ -2,8 +2,8 @@ local utils = require("lf.utils")
 
 local ok, terminal = pcall(require, "toggleterm")
 if not ok then
-    utils.err("toggleterm.nvim must be installed to use this program")
-    return
+	utils.err("toggleterm.nvim must be installed to use this program")
+	return
 end
 
 local cmd = vim.cmd
@@ -39,23 +39,23 @@ Lf.__index = Lf
 ---@private
 ---Setup `toggleterm`'s `Terminal`
 local function setup_term()
-    terminal.setup({
-        size = function(term)
-            if term.direction == "horizontal" then
-                return o.lines * 0.4
-            elseif term.direction == "vertical" then
-                return o.columns * 0.5
-            end
-        end,
-        hide_numbers = true,
-        shade_filetypes = {},
-        shade_terminals = false,
-        start_in_insert = true,
-        insert_mappings = false,
-        terminal_mappings = true,
-        persist_mode = false,
-        persist_size = false,
-    })
+	terminal.setup({
+		size = function(term)
+			if term.direction == "horizontal" then
+				return o.lines * 0.4
+			elseif term.direction == "vertical" then
+				return o.columns * 0.5
+			end
+		end,
+		hide_numbers = true,
+		shade_filetypes = {},
+		shade_terminals = false,
+		start_in_insert = true,
+		insert_mappings = false,
+		terminal_mappings = true,
+		persist_mode = false,
+		persist_size = false,
+	})
 end
 
 ---Setup a new instance of `Lf`
@@ -64,91 +64,91 @@ end
 ---@param config? Lf.Config
 ---@return Lf
 function Lf:new(config)
-    if config then
-        self.cfg = Config:merge(config)
-    else
-        self.cfg = Config.data
-    end
+	if config then
+		self.cfg = Config:merge(config)
+	else
+		self.cfg = Config.data
+	end
 
-    self:__set_argv()
-    self.bufnr = 0
-    self.view_idx = 1
-    self.action = self.cfg.default_action
-    -- Needs to be grabbed here before the terminal buffer is created
-    self.signcolumn = o.signcolumn
+	self:__set_argv()
+	self.bufnr = 0
+	self.view_idx = 1
+	self.action = self.cfg.default_action
+	-- Needs to be grabbed here before the terminal buffer is created
+	self.signcolumn = o.signcolumn
 
-    setup_term()
-    self:__create_term()
+	setup_term()
+	self:__create_term()
 
-    return self
+	return self
 end
 
 ---@private
 ---Create the `Terminal` and set it o `Lf.term`
 function Lf:__create_term()
-    self.term = Terminal:new({
-        cmd = self.cfg.default_cmd,
-        dir = self.cfg.dir,
-        direction = self.cfg.direction,
-        winblend = self.cfg.winblend,
-        close_on_exit = true,
-        hidden = false,
-        clear_env = self.cfg.env.clear,
-        highlights = self.cfg.highlights,
-        display_name = "Lf",
-        count = self.cfg.count,
-        float_opts = {
-            border = self.cfg.border,
-            width = self.cfg.width,
-            height = self.cfg.height,
-            winblend = self.cfg.winblend,
-        },
-    })
+	self.term = Terminal:new({
+		cmd = self.cfg.default_cmd,
+		dir = self.cfg.dir,
+		direction = self.cfg.direction,
+		winblend = self.cfg.winblend,
+		close_on_exit = true,
+		hidden = false,
+		clear_env = self.cfg.env.clear,
+		highlights = self.cfg.highlights,
+		display_name = "Lf",
+		count = self.cfg.count,
+		float_opts = {
+			border = self.cfg.border,
+			width = self.cfg.width,
+			height = self.cfg.height,
+			winblend = self.cfg.winblend,
+		},
+	})
 end
 
 ---Start the underlying terminal
 ---@param path? string path where `Lf` starts (reads from `Config` if none, else CWD)
 function Lf:start(path)
-    self:__open_in(path or self.cfg.dir)
-    self:__set_cmd_wrapper()
+	self:__open_in(path or self.cfg.dir)
+	self:__set_cmd_wrapper()
 
-    if self.cfg.hijack_netrw then
-        -- Open in current window (replace netrw buffer)
-        self.bufnr = api.nvim_get_current_buf()
-        self.winid = api.nvim_get_current_win()
-        
-        -- Configure buffer for terminal
-        fn.termopen(self.term.cmd, {
-            on_exit = function(_, _, _)
-                self:__callback_hijack()
-            end
-        })
-        cmd("startinsert")
-        
-        -- Set buffer options
-        vim.bo[self.bufnr].bufhidden = "wipe"
-        vim.bo[self.bufnr].filetype = "lf"
-        
-        -- Setup mappings
-        if self.cfg.mappings then
-             -- Hijack mode needs simpler mappings since it's a raw terminal
-             -- We can reuse the callback logic but need to adapt it
-        end
-    else
-        -- Standard floating window (toggleterm)
-        self.term.on_open = function(term)
-            self:__on_open(term)
-        end
+	if self.cfg.hijack_netrw then
+		-- Open in current window (replace netrw buffer)
+		self.bufnr = api.nvim_get_current_buf()
+		self.winid = api.nvim_get_current_win()
 
-        self.term.on_exit = function(term, _, _, _)
-            self:__callback(term)
-            uv.fs_unlink(self.tmp_id)
-            uv.fs_unlink(self.tmp_lastdir)
-            uv.fs_unlink(self.tmp_sel)
-        end
+		-- Configure buffer for terminal
+		fn.termopen(self.term.cmd, {
+			on_exit = function(_, _, _)
+				self:__callback_hijack()
+			end,
+		})
+		cmd("startinsert")
 
-        self.term:open()
-    end
+		-- Set buffer options
+		vim.bo[self.bufnr].bufhidden = "wipe"
+		vim.bo[self.bufnr].filetype = "lf"
+
+		-- Setup mappings
+		if self.cfg.mappings then
+			-- Hijack mode needs simpler mappings since it's a raw terminal
+			-- We can reuse the callback logic but need to adapt it
+		end
+	else
+		-- Standard floating window (toggleterm)
+		self.term.on_open = function(term)
+			self:__on_open(term)
+		end
+
+		self.term.on_exit = function(term, _, _, _)
+			self:__callback(term)
+			uv.fs_unlink(self.tmp_id)
+			uv.fs_unlink(self.tmp_lastdir)
+			uv.fs_unlink(self.tmp_sel)
+		end
+
+		self.term:open()
+	end
 end
 
 ---@private
@@ -156,123 +156,110 @@ end
 ---@param path? string
 ---@return Lf?
 function Lf:__open_in(path)
-    if path == "gwd" or path == "git_dir" then
-        path = utils.git_dir()
-    end
-    path = fn.expand((path == "" or path == nil) and "%:p:h" or path)
+	if path == "gwd" or path == "git_dir" then
+		path = utils.git_dir()
+	end
+	path = fn.expand((path == "" or path == nil) and "%:p:h" or path)
 
-    local built = path
-    local stat = uv.fs_stat(path)
-    if not type(stat) == "table" then
-        local cwd = uv.cwd()
-        stat = uv.fs_stat(cwd)
-        built = cwd
-    end
+	local built = path
+	local stat = uv.fs_stat(path)
+	if not type(stat) == "table" then
+		local cwd = uv.cwd()
+		stat = uv.fs_stat(cwd)
+		built = cwd
+	end
 
-    -- Should be fine, but just checking
-    if stat and stat.type ~= "directory" then
-        built = vim.fs.dirname(built)
-    end
+	-- Should be fine, but just checking
+	if stat and stat.type ~= "directory" then
+		built = vim.fs.dirname(built)
+	end
 
-    self.term.dir = built
-    self.curfile = fn.expand("%:p")
+	self.term.dir = built
+	self.curfile = fn.expand("%:p")
 
-    return self
+	return self
 end
 
 ---@private
 ---Wrap the default command to write the selected files to a temporary file
 ---@return Lf
 function Lf:__set_cmd_wrapper()
-    self.tmp_sel = os.tmpname()
-    self.tmp_lastdir = os.tmpname()
-    self.tmp_id = os.tmpname()
+	self.tmp_sel = os.tmpname()
+	self.tmp_lastdir = os.tmpname()
+	self.tmp_id = os.tmpname()
 
-    local open_on = self.term.dir
-    if
-        self.cfg.focus_on_open
-        and vim.fs.dirname(self.curfile) == self.term.dir
-    then
-        open_on = self.curfile
-    end
+	local open_on = self.term.dir
+	if self.cfg.focus_on_open and vim.fs.dirname(self.curfile) == self.term.dir then
+		open_on = self.curfile
+	end
 
-    -- command lf -command '$printf $id > '"$fid"'' -last-dir-path="$tmp" "$@"
-    self.term.cmd =
-        ([[%s -command='$printf $id > %s' -last-dir-path='%s' -selection-path='%s' '%s']])
-        :format(
-            self.term.cmd,
-            self.tmp_id,
-            self.tmp_lastdir,
-            self.tmp_sel,
-            open_on
-        )
-    return self
+	-- command lf -command '$printf $id > '"$fid"'' -last-dir-path="$tmp" "$@"
+	self.term.cmd = ([[%s -command='$printf $id > %s' -last-dir-path='%s' -selection-path='%s' '%s']]):format(
+		self.term.cmd,
+		self.tmp_id,
+		self.tmp_lastdir,
+		self.tmp_sel,
+		open_on
+	)
+	return self
 end
 
 ---@private
 ---On open closure to run in the `Terminal`
 ---@param term Terminal
 function Lf:__on_open(term)
-    self.bufnr = term.bufnr
-    self.winid = term.window
+	self.bufnr = term.bufnr
+	self.winid = term.window
 
-    cmd("silent! doautocmd User LfTermEnter")
+	cmd("silent! doautocmd User LfTermEnter")
 
-    -- Wrap needs to be set, otherwise the window isn't aligned on resize
-    api.nvim_win_call(self.winid, function()
-        vim.wo.showbreak = "NONE"
-        vim.wo.wrap = true
-        vim.wo.sidescrolloff = 0
-        vim.wo.scrolloff = 0
-        vim.wo.scrollbind = false
-    end)
+	-- Wrap needs to be set, otherwise the window isn't aligned on resize
+	api.nvim_win_call(self.winid, function()
+		vim.wo.showbreak = "NONE"
+		vim.wo.wrap = true
+		vim.wo.sidescrolloff = 0
+		vim.wo.scrolloff = 0
+		vim.wo.scrollbind = false
+	end)
 
-    if self.cfg.tmux then
-        utils.tmux(true)
-    end
+	if self.cfg.tmux then
+		utils.tmux(true)
+	end
 
-    -- Not sure if this works
-    if self.cfg.mappings then
-        if self.cfg.escape_quit then
-            vim.keymap.set(
-                "t",
-                "<Esc>",
-                "<Cmd>q<CR>",
-                {buffer = self.bufnr, desc = "Exit Lf"}
-            )
-        end
+	-- Not sure if this works
+	if self.cfg.mappings then
+		if self.cfg.escape_quit then
+			vim.keymap.set("t", "<Esc>", "<Cmd>q<CR>", { buffer = self.bufnr, desc = "Exit Lf" })
+		end
 
-        for key, mapping in pairs(self.cfg.default_actions) do
-            vim.keymap.set("t", key, function()
-                -- Change default_action for easier reading in the callback
-                self.action = mapping
+		for key, mapping in pairs(self.cfg.default_actions) do
+			vim.keymap.set("t", key, function()
+				-- Change default_action for easier reading in the callback
+				self.action = mapping
 
-                local res = utils.read_file(self.tmp_id)
-                self.id = tonumber(res)
+				local res = utils.read_file(self.tmp_id)
+				self.id = tonumber(res)
 
-                fn.system({"lf", "-remote", ("send %d open"):format(self.id)})
-            end, {noremap = true, buffer = self.bufnr, desc = ("Lf %s"):format(mapping)})
-        end
+				fn.system({ "lf", "-remote", ("send %d open"):format(self.id) })
+			end, { noremap = true, buffer = self.bufnr, desc = ("Lf %s"):format(mapping) })
+		end
 
-        if self.cfg.layout_mapping then
-            vim.keymap.set("t", self.cfg.layout_mapping, function()
-                api.nvim_win_set_config(self.winid, utils.get_view(
-                    self.cfg.views[self.view_idx],
-                    self.bufnr,
-                    self.signcolumn
-                ))
-                self.view_idx = self.view_idx < #self.cfg.views
-                    and self.view_idx + 1
-                    or 1
-            end)
-        end
-    end
+		if self.cfg.layout_mapping then
+			vim.keymap.set("t", self.cfg.layout_mapping, function()
+				api.nvim_win_set_config(
+					self.winid,
+					utils.get_view(self.cfg.views[self.view_idx], self.bufnr, self.signcolumn)
+				)
+				self.view_idx = self.view_idx < #self.cfg.views and self.view_idx + 1 or 1
+			end)
+		end
+	end
 
-    -- Don't know why whenever wrap is set in the terminal, a weird resize happens.
-    -- Because of that, this is needed here.
-    vim.defer_fn(function()
-        cmd("silent! doautoall VimResized")
-    end, 800)
+	-- Don't know why whenever wrap is set in the terminal, a weird resize happens.
+	-- Because of that, this is needed here.
+	vim.defer_fn(function()
+		cmd("silent! doautoall VimResized")
+	end, 800)
 end
 
 ---@private
@@ -280,105 +267,107 @@ end
 ---
 ---@param term Terminal
 function Lf:__callback(term)
-    if self.cfg.tmux then
-        utils.tmux(false)
-    end
+	if self.cfg.tmux then
+		utils.tmux(false)
+	end
 
-    if
-        (self.action == "cd" or self.action == "lcd")
-        and uv.fs_stat(self.tmp_lastdir)
-    then
-        local last_dir = utils.read_file(self.tmp_lastdir)
-        if last_dir ~= nil and last_dir ~= uv.cwd() then
-            cmd(("%s %s"):format(self.action, fn.fnameescape(last_dir)))
-            return
-        end
-    elseif uv.fs_stat(self.tmp_sel) then
-        term:close()
-        for fname in io.lines(self.tmp_sel) do
-            local stat = uv.fs_stat(fname)
-            if type(stat) == "table" then
-                local fesc = fn.fnameescape(fname)
-                cmd(("%s %s"):format(self.action, fesc))
-                local args = table.concat(self.arglist, " ")
-                if string.len(args) > 0 then
-                  cmd.argadd(args)
-                  cmd.argdedupe()
-                end
-                self:__set_argv()
-            end
-        end
-    end
+	if (self.action == "cd" or self.action == "lcd") and uv.fs_stat(self.tmp_lastdir) then
+		local last_dir = utils.read_file(self.tmp_lastdir)
+		if last_dir ~= nil and last_dir ~= uv.cwd() then
+			cmd(("%s %s"):format(self.action, fn.fnameescape(last_dir)))
+			return
+		end
+	elseif uv.fs_stat(self.tmp_sel) then
+		term:close()
+		for fname in io.lines(self.tmp_sel) do
+			local stat = uv.fs_stat(fname)
+			if type(stat) == "table" then
+				local fesc = fn.fnameescape(fname)
+				cmd(("%s %s"):format(self.action, fesc))
+				local args = table.concat(self.arglist, " ")
+				if string.len(args) > 0 then
+					cmd.argadd(args)
+					cmd.argdedupe()
+				end
+				self:__set_argv()
+			end
+		end
+	end
 
-    -- Reset the action
-    vim.defer_fn(function()
-        self.action = self.cfg.default_action
-    end, 1)
+	-- Reset the action
+	vim.defer_fn(function()
+		self.action = self.cfg.default_action
+	end, 1)
 end
 
 ---@private
 ---Callback for hijacked netrw instance
 function Lf:__callback_hijack()
-    if self.cfg.tmux then
-        utils.tmux(false)
-    end
+	if self.cfg.tmux then
+		utils.tmux(false)
+	end
 
-    -- Process selection just like normal callback
-    if uv.fs_stat(self.tmp_sel) then
-        -- Read selections
-        local lines = {}
-        for line in io.lines(self.tmp_sel) do
-            table.insert(lines, line)
-        end
-        
-        if #lines > 0 then
-            -- We have selections, open them
-            -- Since we are in the terminal buffer, we need to edit the first file
-            -- to replace the terminal, then add the rest
-            
-            local first = table.remove(lines, 1)
-            cmd(("edit %s"):format(fn.fnameescape(first)))
-            
-            -- Add remaining files to arglist if any
-            if #lines > 0 then
-                for _, fname in ipairs(lines) do
-                    cmd.argadd(fn.fnameescape(fname))
-                end
-            end
-        else
-            -- No selection (quit), close buffer which closes window if it's the only one
-             if #api.nvim_list_wins() == 1 then
-                cmd("quit")
-            else
-                -- If there are other windows, just wipe this buffer to close the "explorer"
-                cmd("bdelete!") 
-            end
-        end
-    else
-        -- Exit code/Signal logic? mostly just quit
-         if #api.nvim_list_wins() == 1 then
-            cmd("quit")
-        else
-            cmd("bdelete!") 
-        end
-    end
-    
-    -- Cleanup temp files
-    uv.fs_unlink(self.tmp_id)
-    uv.fs_unlink(self.tmp_lastdir)
-    uv.fs_unlink(self.tmp_sel)
+	-- Process selection just like normal callback
+	if uv.fs_stat(self.tmp_sel) then
+		-- Read selections
+		local lines = {}
+		for line in io.lines(self.tmp_sel) do
+			table.insert(lines, line)
+		end
+
+		if #lines > 0 then
+			-- We have selections, open them
+			-- Since we are in the terminal buffer, we need to edit the first file
+			-- to replace the terminal, then add the rest
+
+			local first = table.remove(lines, 1)
+			cmd(("edit %s"):format(fn.fnameescape(first)))
+
+			-- Add remaining files to arglist if any
+			if #lines > 0 then
+				for _, fname in ipairs(lines) do
+					cmd.argadd(fn.fnameescape(fname))
+				end
+			end
+		else
+			-- No selection (quit), close buffer which closes window if it's the only one
+			if #api.nvim_list_wins() == 1 then
+				cmd("quit")
+			else
+				-- If there are other windows, just wipe this buffer to close the "explorer"
+				cmd("bdelete!")
+			end
+		end
+	else
+		-- Exit code/Signal logic? mostly just quit
+		if #api.nvim_list_wins() == 1 then
+			cmd("quit")
+		else
+			cmd("bdelete!")
+		end
+	end
+
+	-- Cleanup temp files
+	uv.fs_unlink(self.tmp_id)
+	uv.fs_unlink(self.tmp_lastdir)
+	uv.fs_unlink(self.tmp_sel)
 end
 
 ---@private
 ---Set the arglist value
 function Lf:__set_argv()
-    local args = {}
-    for _, arg in ipairs(fn.argv()) do
-        if api.nvim_buf_is_loaded(fn.bufnr(arg)) then
-            table.insert(args, uv.fs_realpath(arg))
-        end
-    end
-    self.arglist = args
+	local args = {}
+	for _, arg in ipairs(fn.argv()) do
+		local realpath = uv.fs_realpath(arg)
+		if api.nvim_buf_is_loaded(fn.bufnr(arg)) then
+			if realpath then
+				table.insert(args, uv.fs_realpath(arg))
+			else
+				api.nvim_buf_delete(fn.bufnr(arg), { force = false })
+			end
+		end
+	end
+	self.arglist = args
 end
 
 return Lf
